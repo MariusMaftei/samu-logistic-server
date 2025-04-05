86cdc6783b050ae079a0bc4a09d34b92777b740d
Marius Maftei <mafteimarius93@gmail.com>
2025-04-05 20:08:18 +0300

    test server 1

--- config/database.js
+++ config/database.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Update to use your RDS instance endpoint, not localhost
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_URL, // Keep this if you're using DATABASE_URL for the connection string
+  process.env.DATABASE_USERNAME, // Keep this or ensure the .env file has the correct username
+  process.env.DATABASE_PASSWORD, // Make sure the password in .env is correct
   {
     dialect: "mysql",
     host: process.env.DATABASE_HOST,
@@ -16,3 +17,5 @@ const sequelize = new Sequelize(
     },
   }
 );
+
+export default sequelize;
