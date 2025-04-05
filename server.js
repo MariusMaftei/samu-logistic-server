440e6c0bee6a2280e48d7f03d4a32c2a529effd2
Marius Maftei <mafteimarius93@gmail.com>
2025-04-05 13:34:48 +0300

    updated

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from "express";
 import cors from "cors";
 import sequelize from "./config/database.js";
-import authRoute from "./routes/auth.js";
-import postRoute from "./routes/post.js";
-import setupAssociations from "./models/setupAssociations.js";
 import entryRoute from "./routes/entry.js";
 
 const app = express();
@@ -11,8 +8,6 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-
-setupAssociations();
 
 app.get("/", (req, res) => {
   res.send(`<h1>Internal Server Error</h1> 
@@ -22,8 +17,6 @@ app.get("/", (req, res) => {
     <p>More information about this error may be available in the server error log.</p>`);
 });
 app.use("/entry", entryRoute);
-app.use("/auth", authRoute);
-app.use("/posts", postRoute);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
