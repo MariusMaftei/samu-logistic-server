4ed81a6e8475d93ebae53ee18b548aa54b37a10a
Marius Maftei <mafteimarius93@gmail.com>
2025-04-05 13:49:32 +0300

    updated db

--- config/database.js
+++ config/database.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
+  process.env.DATABASE_URL ||
+    `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:3306/${process.env.DATABASE_NAME}`,
   {
     dialect: "mysql",
-    host: "localhost",
+    define: {
+      timestamps: false, // Optional: depends on whether you want timestamps
+    },
   }
 );
 
